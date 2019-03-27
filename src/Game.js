31ff7890c92c7889aa0fe53d92c492386f835076
purplesands <mullenjoe13@gmail.com>
2019-03-26 20:20:54 -0400

    refactor

--- src/Game.js
+++ src/Game.js
@@ -7,79 +7,79 @@ import Score from './Score';
 class Game extends Component {
 
   state = {
-    questions: [],
-    answers: {},
-    currentQuestion: {},
-    currentAnswers: {},
+    // questions: [],
+    // answers: {},
+    // currentQuestion: {},
+    // currentAnswers: {},
+    round: 1,
     userInput: '',
-    points: 0,
     timer:10
   }
 
-  fetchQuestions = () => {
-    fetch('https://wicked-panda-94.localtunnel.me/questions')
-    .then(r => r.json())
-    .then(q => {
-      let shuffled = q.map(x => { return {data: x, srt: Math.random()}})
-      .sort((a,b) => {return a.srt - b.srt})
-      .map(x => x.data);
-      this.setState({questions: shuffled.slice(0,10)})
-    })
-  }
+  // fetchQuestions = () => {
+  //   fetch('http://localhost:3000/questions')
+  //   .then(r => r.json())
+  //   .then(q => {
+  //     let shuffled = q.map(x => { return {data: x, srt: Math.random()}})
+  //     .sort((a,b) => {return a.srt - b.srt})
+  //     .map(x => x.data);
+  //     this.setState({questions: shuffled.slice(0,10)})
+  //   })
+  // }
+  //
+  // fetchAnswers = () => {
+  //   fetch('http://localhost:3000/answers')
+  //   .then(r => r.json())
+  //   .then(a => {
+  //     let arr = this.matchAnswers(a, this.state.questions)
+  //     this.setState({answers: arr},
+  //       this.setQuestion
+  //     )
+  //   })
+  // }
 
-  fetchAnswers = () => {
-    fetch('https://wicked-panda-94.localtunnel.me/answers')
-    .then(r => r.json())
-    .then(a => {
-      let arr = this.matchAnswers(a, this.state.questions)
-      this.setState({answers: arr},
-        this.setQuestion
-      )
-    })
-  }
-
-  matchAnswers = (answers, questions) => {
-    let answerSet = {}
-    for (var i = 0; i < questions.length; i++) {
-      answerSet[questions[i].id] = []
-      for (var y = 0; y < answers.length; y++) {
-        if (answers[y].question_id === questions[i].id) {
-          answerSet[questions[i].id].push(answers[y])
-        }
-      }
-    }
-    return answerSet
-  }
+  // matchAnswers = (answers, questions) => {
+  //   let answerSet = {}
+  //   for (var i = 0; i < questions.length; i++) {
+  //     answerSet[questions[i].id] = []
+  //     for (var y = 0; y < answers.length; y++) {
+  //       if (answers[y].question_id === questions[i].id) {
+  //         answerSet[questions[i].id].push(answers[y])
+  //       }
+  //     }
+  //   }
+  //   return answerSet
+  // }
 
   nextRound=()=>{
     // keeps track of timer, and calls setQuestion and setAnswers at a certain time
   }
 
-  setQuestion = () => {
-    // takes this.state.questions
-    // removes one question out and sets state of currentQuestion to that question
-    // is called by some kind of timer
-    let questions = [...this.state.questions]
-    let currentQuestion = questions[0]
-    questions.shift()
-    console.log('cq', currentQuestion)
-    this.setState({
-      questions: questions,
-      currentQuestion: currentQuestion
-    }, this.setAnswers)
-  }
+  // setQuestion = () => {
+  //   // takes this.state.questions
+  //   // removes one question out and sets state of currentQuestion to that question
+  //   // is called by some kind of timer
+  //   let questions = [...this.state.questions]
+  //   let currentQuestion = questions[0]
+  //   questions.shift()
+  //   console.log('cq', currentQuestion)
+  //   this.setState({
+  //     questions: questions,
+  //     currentQuestion: currentQuestion
+  //   }, this.setAnswers)
+  // }
 
-  setAnswers = () => {
-    let currentQuestion = this.state.currentQuestion
-    let answers = this.state.answers
-    let shuffled = answers[currentQuestion.id].map(x => { return {data: x, srt: Math.random()}})
-    .sort((a,b) => {return a.srt - b.srt})
-    .map(x => x.data);
-    this.setState({currentAnswers: shuffled.slice(0,3)})
-    // takes this.state.currentQuestion
-    // matches that question with matching answers
-    // takes 3 of those answers at random and assigns to this.state.currentAnswer
-  }
+  // setAnswers = () => {
+  //   let currentQuestion = this.state.currentQuestion
+  //   let answers = this.state.answers
+  //   let shuffled = answers[currentQuestion.id].map(x => { return {data: x, srt: Math.random()}})
+  //   .sort((a,b) => {return a.srt - b.srt})
+  //   .map(x => x.data);
+  //   this.setState({currentAnswers: shuffled.slice(0,3)})
+  //   // takes this.state.currentQuestion
+  //   // matches that question with matching answers
+  //   // takes 3 of those answers at random and assigns to this.state.currentAnswer
+  // }
 
   getUserInput = (input) => {
     this.setState({
@@ -88,29 +88,22 @@ class Game extends Component {
   }
 
   componentDidMount = () => {
-    this.fetchQuestions()
-    this.fetchAnswers()
   }
 
-  addPoint = () => {
-    let points = 1
-    points++
-    console.log(points)
-  }
 
   renderGame = () => {
-    if (this.state.currentAnswers.length !== undefined) {
+    if (this.props.currentAnswers.length !== undefined) {
       return (
         <div>
         <p>{this.state.timer} seconds left</p>
         <Score />
         <QuestionScreen
-          currentQuestion={this.state.currentQuestion}
+          currentQuestion={this.props.currentQuestion}
           getUserInput={this.getUserInput}
           />
         <AnswerScreen
-          currentAnswers={this.state.currentAnswers}
-          currentQuestion={this.state.currentQuestion}
+          currentAnswers={this.props.currentAnswers}
+          currentQuestion={this.props.currentQuestion}
           userInput={this.state.userInput}
           addPoint={this.addPoint}
           />
@@ -119,7 +112,7 @@ class Game extends Component {
     } else {
       return (
         <QuestionScreen
-          currentQuestion={this.state.currentQuestion}
+          currentQuestion={this.props.currentQuestion}
           getUserInput={this.getUserInput}
           />
       )
@@ -134,7 +127,7 @@ class Game extends Component {
 
     return (
       <div className="Game">
-        <button onClick={this.setQuestion}>next Q</button>
+        <button onClick={this.props.setQuestion}>next Q</button>
         {this.renderGame()}
       </div>
     );
--- src/GameList.js
+++ src/GameList.js
@@ -5,24 +5,117 @@ import Game from './Game';
 class GameList extends Component {
 
   state= {
-    game: false
+    questions: [],
+    answers: {},
+    currentQuestion: {},
+    currentAnswers: {},
+    currentGame: null
   }
 
   startGame=()=>{
-    this.setState({
-      game: true
+    fetch('http://localhost:3000/game_instances', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        game_round: 1,
+        game_status: "in progress"
+      })
+    }).then(r=>r.json())
+    .then(r=>this.setState({
+      currentGame: r.id
+    }))
+  }
+
+  fetchQuestions = () => {
+    fetch('http://localhost:3000/questions')
+    .then(r => r.json())
+    .then(q => {
+      let shuffled = q.map(x => { return {data: x, srt: Math.random()}})
+      .sort((a,b) => {return a.srt - b.srt})
+      .map(x => x.data);
+      this.setState({questions: shuffled.slice(0,10)},
+    this.fetchAnswers)
     })
   }
 
-  renderGame=()=>{
-    if (this.state.game) {
-    return (
-      <Game />
-    )
-  } else {
-    return null
+  fetchAnswers = () => {
+    fetch('http://localhost:3000/answers')
+    .then(r => r.json())
+    .then(a => {
+      let arr = this.matchAnswers(a, this.state.questions)
+      this.setState({answers: arr},
+        this.setQuestion
+      )
+    })
   }
-}
+
+  matchAnswers = (answers, questions) => {
+    let answerSet = {}
+    for (var i = 0; i < questions.length; i++) {
+      answerSet[questions[i].id] = []
+      for (var y = 0; y < answers.length; y++) {
+        if (answers[y].question_id === questions[i].id) {
+          answerSet[questions[i].id].push(answers[y])
+        }
+      }
+    }
+    return answerSet
+  }
+
+  setQuestion = () => {
+    // takes this.state.questions
+    // removes one question out and sets state of currentQuestion to that question
+    // is called by some kind of timer
+    let questions = [...this.state.questions]
+    let currentQuestion = questions[0]
+    questions.shift()
+    console.log(this.state.questions)
+    console.log('cq', currentQuestion)
+    this.setState({
+      questions: questions,
+      currentQuestion: currentQuestion
+    }, this.setAnswers)
+  }
+
+  setAnswers = () => {
+    let currentQuestion = this.state.currentQuestion
+    let answers = this.state.answers
+    let shuffled = answers[currentQuestion.id].map(x => { return {data: x, srt: Math.random()}})
+    .sort((a,b) => {return a.srt - b.srt})
+    .map(x => x.data);
+    console.log('shuffled', shuffled.slice(0,3))
+
+    this.setState({currentAnswers: shuffled.slice(0,3)})
+    // takes this.state.currentQuestion
+    // matches that question with matching answers
+    // takes 3 of those answers at random and assigns to this.state.currentAnswer
+  }
+
+  componentDidMount=()=>{
+    this.fetchQuestions()
+  }
+
+  renderGame=()=>{
+    if (this.state.currentGame) {
+      return (
+        <Game
+        questions={this.state.questions}
+        answers={this.state.answers}
+        currentQuestion={this.state.currentQuestion}
+        currentAnswers={this.state.currentAnswers}
+        setQuestion={this.setQuestion}
+        />
+      )
+    } else {
+      return null
+    }
+
+  }
+
+
 
   render() {
     return (
